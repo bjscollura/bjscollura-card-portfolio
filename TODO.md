6665e569dee6961506f78f1080d41311b1338ab1
Brian Collura <bjscollura@gmail.com>
2023-11-25 18:26:36 -0500

    Label transparency event complete

--- TODO.md
+++ TODO.md
@@ -1,7 +1,8 @@
+Finish label for theme picker - final text and polish
+
 - remove / decide on Tangerine and other extra Google fonts
 - resize social and theme buttons/links to minimum 40x40px
 - Different icon for each theme? 1-6?
-- Add titles to button / click action (tooltip?)
 - add local cookie to remember theme setting, at least between page loads
 - fix a few coloring issues with the themes. And add 1 more theme?
 
--- src/lib/ThemePicker.svelte
+++ src/lib/ThemePicker.svelte
@@ -8,6 +8,10 @@
         displayNumber: 5,
         themeName: themeArray[4] //dice number is base-1
     };
+    let diceLabelWrapperEl;
+    let isLabelVisible = false;
+    let labelTimeoutID;
+    
     function changeTheme() {
         let currentTheme = document.body.className;
         let randNum = Math.floor(Math.random() * themeArray.length);
@@ -21,77 +25,110 @@
         
         props.displayNumber = randNum + 1; //change number for Dice.svelte to display, base 1 vs base 0
         props.themeName = themeArray[randNum];
+        
+        //Label
+        window.clearTimeout(labelTimeoutID); //reset timout from previous clicks within 2s
+        isLabelVisible = false;
+        window.requestAnimationFrame(() => isLabelVisible = true);
+        window.requestAnimationFrame(() => diceLabelWrapperEl.classList.remove("animate"));
+        window.requestAnimationFrame(() => diceLabelWrapperEl.classList.add("animate")); //reapply animate class
+        // if (isLabelVisible) { diceLabelWrapperEl.style.animationPlayState = 'paused' }
+        labelTimeoutID = window.setTimeout(() => {
+            isLabelVisible = false;
+            console.log('time!');
+        }, 2000);
     }
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="diceWrapper">
-    <div on:click={changeTheme}>
+    <div class='svgWrapper' on:click={changeTheme}>
         <svelte:component this={Dice} {...props} />
     </div>
-    <div class="diceLabel {labelSide}">
-        {props.themeName}
-    </div>
+    {#if isLabelVisible}
+        <div class="diceLabelWrapper" bind:this={diceLabelWrapperEl}>
+            <div class="diceLabel {labelSide}">
+                {props.themeName}
+            </div>
+        </div>
+    {/if}
 </div>
 
 <style>
-    div {
+    div.svgWrapper {
         display: flex;
         flex-direction: column;
         justify-content: center;
         cursor: pointer;
-        /* outline: 1px solid red; */
     }
     p {
         color: var(--color-accent)
     }
-    
+    @keyframes labelAppear {
+        0% {
+            opacity: 0;
+        }
+        10% {
+            opacity: 1;
+        }
+        50% {
+            opacity: 1;
+        }
+        100% {
+            opacity: 0;
+        }
+    }
     .diceWrapper {
         position: relative;
     }
-    .diceLabel.bottom {
+    .diceLabel {
         position: absolute;
-        top: 100%;
-        left: -8px;
-        margin-top: 0.5rem;
-        padding: 0.5rem;
-        border-radius: 7px;
         background-color: var(--color-accent);
         color: var(--color-bg);
+        left: -8px;
+        padding: 0.5rem;
+        border-radius: 7px;
     }
-    .diceLabel.bottom::after {
+    .diceLabel.bottom {
+        top: 100%;
+        margin-top: 0.5rem;
+    }
+    .diceLabel.top {
+        bottom: 100%;
+        margin-bottom: 0.6rem;
+    }
+    .diceLabel::after {
         content: "";
         position: absolute;
-        top: 1px;
         left: 0.75rem;
         width: 0;
         height: 0;
+        /* opacity: 0; */
+    }
+    .diceLabel.bottom::after {
+        top: 1px;
         border: 0.5rem solid transparent;
         border-bottom-color: var(--color-accent);
         border-top-width: 0;
         margin-top: -0.5rem;
     }
-    .diceLabel.top {
-        position: absolute;
-        bottom: 100%;
-        left: -8px;
-        margin-bottom: 0.6rem;
-        padding: 0.5rem;
-        border-radius: 7px;
-        background-color: var(--color-accent);
-        color: var(--color-bg);
-    }
     .diceLabel.top::after {
-        content: "";
-        position: absolute;
         bottom: 1px;
-        left: 0.75rem;
-        width: 0;
-        height: 0;
         border: 0.6rem solid transparent;
         border-top-color: var(--color-accent);
         border-bottom-width: 0;
         margin-bottom: -0.5rem;
     }
+    .diceLabelWrapper {
+        opacity: 1;
+    }
+    .diceLabelWrapper.animate {
+        animation: 2s linear 0s labelAppear;
+        opacity: 0;
+    }
+    /* .diceLabel.animate::after {
+        animation: 2s linear 0s labelAppear;
+        opacity: 0;
+    } */
 </style>
